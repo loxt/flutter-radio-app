6e879c295368d8b76a43aae950012a5352480f69
loxt <emannuelloxt@hotmail.com>
2021-02-12 01:12:32 -0400

    feat(state): fetch api and put inside state

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:ai_radio/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(MyApp());
 }
 
@@ -9,9 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Radio',
-      theme: ThemeData( ),
+      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
       debugShowCheckedModeBanner: false,
-      home: Container(),
+      home: HomePage(),
     );
   }
 }
